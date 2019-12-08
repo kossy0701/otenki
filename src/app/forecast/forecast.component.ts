27e45960d6cac4b241f426d9eb4a44dd2a75d107
kossy0701 <0421kossy0421@gmail.com>
2019-12-08 18:20:52 +0900

    add http communication for forecast component

--- src/app/forecast/forecast.component.ts
+++ src/app/forecast/forecast.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OpenWeatherMapService } from '../services/open-weather-map.service';
+import { OpenWeatherMap } from '../shared/models/open-weather-map';
+
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-forecast',
@@ -6,10 +12,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forecast.component.scss']
 })
 export class ForecastComponent implements OnInit {
+  public currentWeatherObservable: Observable<OpenWeatherMap.Current>;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private openWeatherMapService: OpenWeatherMapService) { }
 
   ngOnInit() {
+    this.currentWeatherObservable =
+      this.route.params.pipe(switchMap((param: { city: string; }) => {
+        return this.openWeatherMapService.current(param.city);
+      }));
   }
 
 }
