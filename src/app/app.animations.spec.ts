1d5dd168bf44887cfb218258521d917b4aef000e
kossy0701 <0421kossy0421@gmail.com>
2019-12-12 22:31:42 +0900

    create slideFadeIn and Fadeout

---
+++ src/app/app.animations.spec.ts
@@ -0,0 +1,7 @@
+import { App.Animations } from './app.animations';
+
+describe('App.Animations', () => {
+  it('should create an instance', () => {
+    expect(new App.Animations()).toBeTruthy();
+  });
+});
