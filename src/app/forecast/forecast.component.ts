4385cb31c67254f39fe93aa096432f1503487b98
kossy0701 <0421kossy0421@gmail.com>
2019-12-14 16:47:04 +0900

    add slidein for forecasts

--- src/app/forecast/forecast.component.ts
+++ src/app/forecast/forecast.component.ts
@@ -6,11 +6,29 @@ import { OpenWeatherMap } from '../shared/models/open-weather-map';
 import { Observable } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 
+import { transition, trigger, useAnimation, query, stagger, style } from '@angular/animations';
+import { slideFadeIn } from '../app.animations';
+
 @Component({
   selector: 'app-forecast',
   templateUrl: './forecast.component.html',
-  styleUrls: ['./forecast.component.scss']
+  styleUrls: ['./forecast.component.scss'],
+  animations: [
+    trigger('slideFade', [
+      transition(':enter', [
+        query('mat-list-item', [
+        style({
+          opacity: 0
+        }),
+          stagger(100, [
+            useAnimation(slideFadeIn)
+          ])
+        ])
+      ])
+    ])
+  ]
 })
+
 export class ForecastComponent implements OnInit {
   public currentWeatherObservable: Observable<OpenWeatherMap.Current>;
   public forecastObservable: Observable<OpenWeatherMap.Forecast>;
